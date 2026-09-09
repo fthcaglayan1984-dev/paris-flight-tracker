6a5fc8cc70eaa0318a795429238ee321d8f1dcc8
fthcaglayan1984-dev <fthcaglayan1984@gmail.com>
2026-09-09 16:01:08 +0300

    Create __init__.py

---
+++ scrapers/__init__.py
@@ -0,0 +1,5 @@
+from .turna_com import TurnaComScraper
+from .enuygun_com import EnuygunComScraper
+from .kiwi_api import KiwiAPIScraper
+
+__all__ = ['TurnaComScraper', 'EnuygunComScraper', 'KiwiAPIScraper']
